b60ebfb7359b0fb97f313f999aff41dc133be38c
Mateusz Ładyko <fratik@fratikbot.pl>
2018-12-25 14:02:20 +0100

    poprawienie czasu w weryfikacji [no to samo]

--- src/main/java/pl/fratik/FratikDev/funkcje/Weryfikacja.java
+++ src/main/java/pl/fratik/FratikDev/funkcje/Weryfikacja.java
@@ -65,6 +65,10 @@ public class Weryfikacja {
                     else data = weryfikacjaInfo.getWeryfikacja();
                     Calendar cal2 = Calendar.getInstance();
                     cal2.setTime(data);
+                    cal2.set(Calendar.HOUR_OF_DAY, 0);
+                    cal2.set(Calendar.MINUTE, 0);
+                    cal2.set(Calendar.SECOND, 0);
+                    cal2.set(Calendar.MILLISECOND, 0);
                     cal2.add(Calendar.DAY_OF_MONTH, 3);
                     Date koniec = Date.from(cal2.toInstant());
                     if (!dzisiaj.equals(koniec)) continue;
